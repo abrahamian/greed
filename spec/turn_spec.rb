bf7b6d6c390176c464cdb3b3e47132d2e83a5d9b
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2014-09-05 15:56:17 -0400

    remove empty line

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,7 +6,6 @@ describe Turn do
   let(:player){double("Some Player")}
   let(:turn){Turn.new({:player=>player})}
 
-
   describe "#player" do
     it "holds a player object" do
       expect(turn.player).to be player
