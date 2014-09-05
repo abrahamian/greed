5a31c84ad6471fef3c04027cff517a7f9ca8ca5c
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2014-09-05 13:01:57 -0400

    test player methods

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 require 'rspec/mocks'
 require_relative '../lib/player'
 
-RSpec.configure do |config|
-  config.mock_framework = :rspec
-end
-
 describe Player do
 
-  let(:mihran) {Player.new({:name=>"Mihran"})}
+  let(:joe) {Player.new({:name=>"Joe"})}
 
   # incoming query message
   describe "#to_s" do
     it "returns player's name" do
-      expect("#{mihran}").to eq "Mihran"
+      expect("#{joe}").to eq "Joe"
     end
   end
 
   # incoming command message
   describe "#score=" do
     it "changes the player's score" do
-      mihran.score += 69
-      expect(mihran.score).to eq 69
+      joe.score += 69
+      expect(joe.score).to eq 69
     end
   end
 
   # outgoing command message
-  it "#roll" do
-    dbl = double("Some dice")
-    expect(dbl).to receive(:roll)
-    mihran.dice = [dbl]
-    mihran.roll(mihran.dice)
+  describe "#roll" do
+    it "sends roll message to all dice passed to it" do
+      dbl1 = double("Some Dice")
+      dbl2 = double("Another Dice")
+      expect(dbl1).to receive(:roll)
+      expect(dbl2).to receive(:roll)
+      joe.dice = [dbl1, dbl2]
+      joe.roll(joe.dice)
+    end
   end
 
 end
